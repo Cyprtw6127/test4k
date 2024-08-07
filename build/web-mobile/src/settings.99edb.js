9ef69f60bc0c381311e28dd8d7028a2e77dd675d
yong <113157944+Cyprtw6127@users.noreply.github.com>
2024-08-07 14:09:40 +0800

    upload test

---
+++ build/web-mobile/src/settings.99edb.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/main.fire",orientation:"",jsList:[],bundleVers:{sp:"d6d94",internal:"d17e2",main:"96200"}};
